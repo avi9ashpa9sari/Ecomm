626f9a9e23f4e7d919dfd16e136b0f04c210056b
avinash <avinash.pansari@preqin.com>
2023-01-27 20:18:00 +0530

    Add project files.

---
+++ Ecomm/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using Ecomm.Data;
+using Ecomm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ecomm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.categories.ToListAsync());
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _context.categories.FirstOrDefaultAsync(x => x.Id == id));
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Category category)
+        {
+            await _context.categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Category category)
+        {
+            var catUpdate = await _context.categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (catUpdate != null)
+            {
+                catUpdate.Title = category.Title;
+                catUpdate.DisplayOrder = category.DisplayOrder;
+                _context.categories.Update(catUpdate);
+                _context.SaveChanges();
+                return Ok("Category Updated");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var catDelete = await _context.categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (catDelete != null)
+            {
+                _context.categories.Remove(catDelete);
+                _context.SaveChanges();
+                return Ok("Category Deleted");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Ecomm/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Ecomm.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Ecomm.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+
+
+
+
+        //public class AppDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+        //{
+        //    public ApplicationDbContext CreateDbContext(string[] args)
+        //    {
+        //        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        //        optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Database = EcommAPI; Trusted_Connection = true");
+
+        //        return new ApplicationDbContext(optionsBuilder.Options);
+        //    }
+        //}
+
+        //protected override void OnConfiguring(DbContextOptions options)
+        //{
+        //    options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=app_db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False");
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer(@"server=(LocalDB)\\MSSQLLocalDB;Database= EcommAPI;Trusted_Connection=True;");
+            }
+        }
+    }
+}
